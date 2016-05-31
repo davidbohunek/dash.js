9db00583884bbd5368cd2493e0857e0723d36df7
seanxe <seanxe@gmail.com>
2016-05-31 11:43:37 +0100

    @seanxe i am the one who knocks

--- samples/develop/main.js
+++ samples/develop/main.js
@@ -1,7 +1,18 @@
 (function(){
-    var url = "http://dash.edgesuite.net/envivio/EnvivioDash3/manifest.mpd";
-    var player = dashjs.MediaPlayer().create();
-    // player.getDebug().setLogToBrowserConsole(false);
-    window.enableProxy = true;
-    player.initialize(document.querySelector("#videoPlayer"), url, true);
+
+    function loadPlayer() {
+        var url = "http://dash.edgesuite.net/envivio/EnvivioDash3/manifest.mpd";
+        var player = dashjs.MediaPlayer().create();
+        player.getDebug().setLogToBrowserConsole(false);
+        window.enableProxy = false;
+        player.initialize(document.querySelector("#videoPlayer"), url, true);
+    }
+
+
+    document.querySelector('#reloadPlayerBtn').addEventListener('click', () => {
+        loadPlayer();
+    }, false);
+
+    loadPlayer();
+
 })();
