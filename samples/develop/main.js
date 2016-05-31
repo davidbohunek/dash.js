3f370115e99335a5fd60ada5275f448a8408d107
David Bohunek <david@bohunek.net>
2016-05-31 11:51:32 +0100

    sync

--- samples/develop/main.js
+++ samples/develop/main.js
@@ -1,7 +1,7 @@
 (function(){
     var url = "http://dash.edgesuite.net/envivio/EnvivioDash3/manifest.mpd";
     var player = dashjs.MediaPlayer().create();
-    // player.getDebug().setLogToBrowserConsole(false);
-    window.enableProxy = true;
+    player.getDebug().setLogToBrowserConsole(false);
     player.initialize(document.querySelector("#videoPlayer"), url, true);
+    player.setAutoSwitchQuality(false);
 })();
